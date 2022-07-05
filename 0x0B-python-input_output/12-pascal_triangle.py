696015f4a551e5c4e22063cdcfa634b0110eb4af
NancyLwambi <nancylwambi@gmail.com>
2022-07-05 12:21:22 -0700

    12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Defines a Pascal's Triangle function."""
-def pascal_triangle(n):
 
+
+def pascal_triangle(n):
     """Represent Pascal's Triangle of size n.
     Returns a list of lists of integers representing the triangle.
     """
